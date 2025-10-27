3f2e1bb47d3310c8354c745be1b6e89785435ad2
Khuslee1 <Khuselee11@gmail.com>
2025-10-27 12:57:05 +0800

    oct27day

--- caru.js
+++ caru.js
@@ -8,52 +8,61 @@ let offset = 0;
 let leftd = 0;
 let rigthd = 0;
 let zuvputin = 0;
-if (zuvputin == 0) {
-  right.disabled = true;
-} else if (zuvputin == 10) {
-  left.disabled = true;
-} else {
-  left.disabled = false;
-  right.disabled = false;
-}
+let int = null;
 box[zuvputin + 1].style.transform = "scale(1.25)";
+box[zuvputin + 1].style.transition = "0.5s";
 box[zuvputin + 2].style.transform = "scale(0.75)";
+box[zuvputin + 2].style.transition = "0.5s";
 
-right.addEventListener("click", () => {
-  offset += 230;
-  boxCont.style.transform = `translateX(${offset}px)`;
-  rigthd++;
-  zuvputin = leftd - rigthd;
-  if (zuvputin == 0) {
-    right.disabled = true;
-  } else if (zuvputin == 10) {
-    left.disabled = true;
-  } else {
-    left.disabled = false;
-    right.disabled = false;
-  }
-  box[zuvputin + 1].style.transform = "scale(1.25)";
-  box[zuvputin].style.transform = "scale(0.75)";
-  box[zuvputin + 2].style.transform = "scale(0.75)";
-  boxCont.style.transition = `0.5s`;
-  box[zuvputin + 1].style.transition = "0.5s";
-  box[zuvputin].style.transition = "0.5s";
-  box[zuvputin + 2].style.transition = "0.5s";
-});
+// if (zuvputin == 0) {
+//   right.disabled = true;
+// } else if (zuvputin == 10) {
+//   left.disabled = true;
+// } else {
+//   left.disabled = false;
+//   right.disabled = false;
+// }
+// box[zuvputin + 1].style.transform = "scale(1.25)";
+// box[zuvputin + 2].style.transform = "scale(0.75)";
 
-left.addEventListener("click", () => {
+// right.addEventListener("click", () => {
+//   offset += 230;
+//   boxCont.style.transform = `translateX(${offset}px)`;
+//   rigthd++;
+//   zuvputin = leftd - rigthd;
+//   if (zuvputin == 0) {
+//     right.disabled = true;
+//   } else if (zuvputin == 10) {
+//     left.disabled = true;
+//   } else {
+//     left.disabled = false;
+//     right.disabled = false;
+//   }
+//   box[zuvputin + 1].style.transform = "scale(1.25)";
+//   box[zuvputin].style.transform = "scale(0.75)";
+//   box[zuvputin + 2].style.transform = "scale(0.75)";
+//   boxCont.style.transition = `0.5s`;
+//   box[zuvputin + 1].style.transition = "0.5s";
+//   box[zuvputin].style.transition = "0.5s";
+//   box[zuvputin + 2].style.transition = "0.5s";
+// });
+
+// left.addEventListener("click", () => {
+int = setInterval(() => {
   offset -= 230;
   boxCont.style.transform = `translateX(${offset}px)`;
-  leftd++;
-  zuvputin = leftd - rigthd;
-  if (zuvputin == 0) {
-    right.disabled = true;
-  } else if (zuvputin == 10) {
-    left.disabled = true;
-  } else {
-    left.disabled = false;
-    right.disabled = false;
-  }
+  // leftd++;
+  // zuvputin = leftd - rigthd;
+  // if (zuvputin == 0) {
+  //   right.disabled = true;
+  // } else if (zuvputin == 10) {
+  //   left.disabled = true;
+  // } else {
+  //   left.disabled = false;
+  //   right.disabled = false;
+  // }
+  console.log(zuvputin);
+  zuvputin++;
   box[zuvputin + 1].style.transform = "scale(1.25)";
   box[zuvputin].style.transform = "scale(0.75)";
   box[zuvputin + 2].style.transform = "scale(0.75)";
@@ -61,7 +70,18 @@ left.addEventListener("click", () => {
   box[zuvputin + 1].style.transition = "0.5s";
   box[zuvputin].style.transition = "0.5s";
   box[zuvputin + 2].style.transition = "0.5s";
-});
+  if (zuvputin == 11) {
+    offset = 0;
+    zuvputin = 0;
+    boxCont.style.transform = `translateX(${offset}px)`;
+    box[zuvputin + 1].style.transform = "scale(1.25)";
+    box[zuvputin + 1].style.transition = "0.5s";
+    box[zuvputin + 2].style.transform = "scale(0.75)";
+    box[zuvputin + 2].style.transition = "0.5s";
+  }
+}, 2000);
+// });
+
 box.forEach((ele) => {
   ele.style.opacity = `${a}`;
   a = a + 0.1;
--- zuram.js
+++ zuram.js
@@ -2,10 +2,22 @@ let count = document.querySelector(".num");
 let start = document.querySelector(".start");
 let res = document.querySelector(".res");
 let box = document.querySelectorAll(".box");
+let move = document.querySelectorAll(".conti");
+let zuram = document.querySelectorAll(".blue");
 let int;
 
-const render = (num) => {
-  box[num].style.backgroundColor = "red";
+score = 0;
+let render = (rand) => {
+  move.forEach((ele, i) => {
+    ele.style.transform = "";
+    zuram[i].style.backgroundImage = `url("./angryb.png")`;
+  });
+  setTimeout(() => {
+    move[rand].style.transform = "translateY(300px)";
+    move[rand].style.transition = "0.5s";
+  }, 1000);
+  move[rand].style.transform = "translateY(-300px)";
+  move[rand].style.transition = "0.5s";
 };
 
 start.addEventListener("click", () => {
@@ -18,8 +30,22 @@ start.addEventListener("click", () => {
     else if (num <= 4 * 1.66) rand = 3;
     else if (num <= 5 * 1.66) rand = 4;
     else rand = 5;
-
-    console.log(rand);
     render(rand);
-  }, 8000);
+  }, 1500);
+});
+
+res.addEventListener("click", () => {
+  clearInterval(int);
+  move.forEach((ele) => {
+    ele.style.transform = "";
+  });
+});
+
+zuram.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    score++;
+    console.log(score);
+    ele.style.backgroundImage = `url("./black.png")`;
+    count.innerHTML = `${score}`;
+  });
 });
