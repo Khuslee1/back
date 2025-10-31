0a9c2d76d6dc53728d43bd1886879f5830a2b303
Khuslee1 <Khuselee11@gmail.com>
2025-10-31 12:30:14 +0800

    oct31day

--- snake.js
+++ snake.js
@@ -4,17 +4,18 @@ let down = document.querySelector(".down");
 let left = document.querySelector(".left");
 let right = document.querySelector(".right");
 let coor = document.querySelectorAll(".coor");
-let text = document.querySelector("h1");
+let text = document.querySelectorAll("h1");
 
 let dir = "d";
 let int;
 
 let border = [];
-let snake = [0, 1, 2, 3, 4];
+let snake = [0, 1];
 let rand = Math.floor(Math.random() * 100);
 let score = 0;
+let winner = false;
 
-coor[rand].style.backgroundColor = "red";
+coor[rand].style.backgroundImage = `url("./baby.png")`;
 
 for (let i = 0; i < 100; i++) {
   if (i < 10 || i > 89 || i % 10 == 0 || i % 10 == 9) {
@@ -25,7 +26,7 @@ for (let i = 0; i < 100; i++) {
 const next = () => {
   switch (dir) {
     case "d":
-      coor[snake[snake.length - 1]].style.backgroundColor = "";
+      coor[snake[snake.length - 1]].style.backgroundImage = "";
       const newPosd = [];
       if (border.includes(snake[0]) && snake[0] >= 90) {
         newPosd.push(snake[0] - 90);
@@ -43,7 +44,7 @@ const next = () => {
       render();
       break;
     case "u":
-      coor[snake[snake.length - 1]].style.backgroundColor = "";
+      coor[snake[snake.length - 1]].style.backgroundImage = "";
       const newPosu = [];
       if (border.includes(snake[0]) && snake[0] <= 9) {
         newPosu.push(snake[0] + 90);
@@ -61,7 +62,7 @@ const next = () => {
       render();
       break;
     case "l":
-      coor[snake[snake.length - 1]].style.backgroundColor = "";
+      coor[snake[snake.length - 1]].style.backgroundImage = "";
       const newPosl = [];
       if (border.includes(snake[0]) && snake[0] % 10 == 0) {
         newPosl.push(snake[0] + 9);
@@ -79,7 +80,7 @@ const next = () => {
       render();
       break;
     case "r":
-      coor[snake[snake.length - 1]].style.backgroundColor = "";
+      coor[snake[snake.length - 1]].style.backgroundImage = "";
       const newPosr = [];
       if (border.includes(snake[0]) && snake[0] % 10 == 9) {
         newPosr.push(snake[0] - 9);
@@ -98,26 +99,13 @@ const next = () => {
       break;
   }
 };
-
 const render = () => {
   snake.forEach((ele) => {
-    coor[ele].style.backgroundColor = "green";
+    coor[ele].style.backgroundImage = `url("./diddy.png")`;
   });
 };
 
 const click = () => {
-  // right.addEventListener("click", () => {
-  //   dir = "r";
-  // });
-  // up.addEventListener("click", () => {
-  //   dir = "u";
-  // });
-  // down.addEventListener("click", () => {
-  //   dir = "d";
-  // });
-  // left.addEventListener("click", () => {
-  //   dir = "l";
-  // });
   document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowDown" && dir !== "u") dir = "d";
     else if (e.key === "ArrowUp" && dir !== "d") dir = "u";
@@ -128,109 +116,24 @@ const click = () => {
 
 const alim = () => {
   rand = Math.floor(Math.random() * 100);
-  coor[rand].style.backgroundColor = "red";
-  if (snake.includes(rand)) {
+  while (snake.includes(rand)) {
     rand = Math.floor(Math.random() * 100);
-    coor[rand].style.backgroundColor = "red";
   }
+  coor[rand].style.backgroundImage = `url("./baby.png")`;
 };
 
+click();
 int = setInterval(() => {
-  click();
   next();
+  if (snake.slice(1).includes(snake[0])) {
+    clearInterval(int);
+    text[1].innerHTML = `Welcome to FREAK OFFS!!!🫦  Score: ${snake.length}`;
+  }
   if (snake[0] == rand) {
-    alim();
     snake.push(snake[snake.length - 1] + 1);
+    alim();
+  }
+  if (winner) {
+    text.inner;
   }
 }, 200);
-
-// for (let i = 0; i <= 143; i++) {
-//   if (i >= 13 && i <= 131 && i % 12 !== 0 && i % 12 !== 11) {
-//     valid.push(i);
-//   }
-// }
-
-// rand = Math.floor(Math.random() * valid.length);
-// coor[valid[rand]].style.backgroundColor = "red";
-
-// right.addEventListener("click", () => {
-//   u = false;
-//   d = false;
-//   r = true;
-//   l = false;
-// });
-// up.addEventListener("click", () => {
-//   u = true;
-//   d = false;
-//   r = false;
-//   l = false;
-// });
-// down.addEventListener("click", () => {
-//   u = false;
-//   d = true;
-//   r = false;
-//   l = false;
-// });
-// left.addEventListener("click", () => {
-//   u = false;
-//   d = false;
-//   r = false;
-//   l = true;
-// });
-
-// int = setInterval(() => {
-//   let tail = snake[snake.length - 1];
-//   if (r) {
-//     coor[tail].style.backgroundColor = "";
-//     // snake[2] = snake[1];
-//     // snake[1] = head;
-//     snake.forEach((ele, i) => {
-//       if (i != 0) {
-//         snake[i + 1] = snake[i];
-//       }
-//     });
-//     snake[1] = head;
-//     head = head + 1;
-//   } else if (d) {
-//     coor[tail].style.backgroundColor = "";
-//     snake.forEach((ele, i) => {
-//       if (i != 0) {
-//         snake[i + 1] = snake[i];
-//       }
-//     });
-//     snake[1] = head;
-//     head = head + 12;
-//   } else if (l) {
-//     coor[tail].style.backgroundColor = "";
-//     snake.forEach((ele, i) => {
-//       if (i != 0) {
-//         snake[i + 1] = snake[i];
-//       }
-//     });
-//     snake[1] = head;
-//     head = head - 1;
-//   } else if (u) {
-//     coor[tail].style.backgroundColor = "";
-//     snake.forEach((ele, i) => {
-//       if (i != 0) {
-//         snake[i + 1] = snake[i];
-//       }
-//     });
-//     snake[1] = head;
-//     head = head - 12;
-//   }
-//   if (head < 13 || head > 131 || head % 12 == 0 || head % 12 == 11) {
-//     clearInterval(int);
-//     text.innerHTML = "game over";
-//   }
-//   coor[head].style.backgroundColor = "blue";
-//   snake.forEach((ele, i) => {
-//     if (i !== 0) coor[snake[i]].style.backgroundColor = "green";
-//   });
-
-//   if (coor[head] == coor[valid[rand]]) {
-//     rand = Math.floor(Math.random() * valid.length);
-//     coor[valid[rand]].style.backgroundColor = "red";
-//     score++;
-//   }
-// }, 1000);
